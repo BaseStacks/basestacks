64e3c061f650b42666b03689026b517d81703874
kenkz447 <kenkz447@gmail.com>
2025-06-11 15:25:08 +0700

    feat: Add eslint-plugin-functional and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import { tanstackConfig } from "@tanstack/eslint-config";
 import tseslint from 'typescript-eslint';
+import functional from "eslint-plugin-functional";
 
 export default [
     ...tanstackConfig,
     ...tseslint.config(
         {
             files: ["**/*.{ts,tsx}"],
+            extends: [
+                functional.configs.stylistic,
+            ],
             rules: {
                 "@typescript-eslint/naming-convention": [
                     "warn",
@@ -14,6 +18,7 @@ export default [
                         "format": ["PascalCase"]
                     }
                 ],
+                "functional/prefer-readonly-type": ["warn", { ignoreCollections: true }],
                 "import/no-default-export": "warn",
             }
         })
