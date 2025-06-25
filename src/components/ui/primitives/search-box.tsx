8e7287463e1594b103abf17d6c0987a34abea593
hoangle217 <hoangxam2@gmail.com>
2025-06-25 14:31:36 +0700

    feat: Enhance SearchBox and BasesSidebar components; implement SearchBoxPopup for improved search functionality and modal provider for consistent modal handling

--- src/components/ui/primitives/search-box.tsx
+++ src/components/ui/primitives/search-box.tsx
@@ -1,15 +1,22 @@
 import { SearchIcon } from "lucide-react";
 import { Input } from "./input";
+import { cn } from "@/lib/utils";
 
 interface SearchBoxProps {
   readonly setSearch: (value: string) => void;
   readonly placeholder?: string;
+  readonly className?: string;
 }
 
 export function SearchBox(props: SearchBoxProps) {
-  const { setSearch, placeholder } = props;
+  const { setSearch, placeholder = "Quick search...", className } = props;
   return (
-    <div className="search-box border border-gray-300 rounded-lg flex items-center pl-2 gap-2 w-fit">
+    <div
+      className={cn(
+        className,
+        "search-box border border-gray-300 rounded-lg flex items-center pl-2 gap-2 w-fit"
+      )}
+    >
       <SearchIcon className="size-4 shrink-0 opacity-50" />
       <Input
         type="text"
--- src/components/ui/widgets/base/BasesSidebar.tsx
+++ src/components/ui/widgets/base/BasesSidebar.tsx
@@ -1,4 +1,18 @@
-import { ChevronDown, ChevronRight, ChevronsLeftIcon, Database, Ellipsis, Grid2x2, LayoutDashboard, Plus, PlusCircle, Search, Square, SquareKanban, Table2, XIcon } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronRight,
+  ChevronsLeftIcon,
+  Database,
+  Ellipsis,
+  Grid2x2,
+  LayoutDashboard,
+  Plus,
+  PlusCircle,
+  Square,
+  SquareKanban,
+  Table2,
+  XIcon,
+} from "lucide-react";
 import { Link } from "@tanstack/react-router";
 import { useMemo, useState } from "react";
 import { Button } from "../../primitives/button";
@@ -6,197 +20,174 @@ import { Label } from "../../primitives/label";
 import { getTextColorClass } from "@/lib/colorUtils";
 import { useBases, useSidebarStatus, useTables, useViews } from "@/states";
 import { useIsMobile } from "@/hooks/ui/useIsMobile";
+import { SearchBoxPopup } from "@/components/shared/SearchBoxPopup";
 
 export function BasesSidebar() {
-    const isMobile = useIsMobile();
-    const sidebarStatus = useSidebarStatus();
-    const { getActiveBase } = useBases();
+  const isMobile = useIsMobile();
+  const sidebarStatus = useSidebarStatus();
+  const { getActiveBase } = useBases();
 
-    const activeBase = getActiveBase();
+  const activeBase = getActiveBase();
 
-    return (
-        <div>
-            <div className="h-16 px-4 flex items-center justify-between">
-                <div className="grow">
-                    <Button size="lg" variant="ghost" className="!px-4">
-                        <Database className={getTextColorClass(activeBase.color)} /> {activeBase.name}
-                        <ChevronDown />
-                    </Button>
-                </div>
-                {
-                    isMobile ? (
-                        <Button
-                            size="icon"
-                            variant="ghost"
-                            onClick={() => sidebarStatus.toggleMobile()}
-                        >
-                            <XIcon />
-                        </Button>
-                    )
-                        : (
-                            <Button
-                                size="icon"
-                                variant="ghost"
-                                onClick={() => sidebarStatus.toggle()}
-                            >
-                                <ChevronsLeftIcon />
-                            </Button>
-                        )
-                }
-            </div>
-            <div className="flex flex-col gap-2 px-4">
-                <div className="py-2 w-full">
-                    <Button variant="outline" className="!px-4 w-full">
-                        <Search />
-                        <span className="grow text-left block font-light">Quick search</span>
-                        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                            <span className="text-xs">Ctrl K</span>
-                        </kbd>
-                    </Button>
-                </div>
-                <div className="w-full flex flex-col gap-1 mb-4">
-                    <Button variant="ghost" className="!px-4 w-full">
-                        <PlusCircle />
-                        <span className="grow text-left ">New table</span>
-                    </Button>
-                    <Button variant="ghost" asChild>
-                        <Link
-                            to="."
-                            search={{ page: 'overview' }}
-                            className="!px-4 w-full"
-                        >
-                            <LayoutDashboard />
-                            <span className="grow text-left block">Overview</span>
-                        </Link>
-                    </Button>
-                </div>
-                <div className="w-full flex flex-col gap-1">
-                    <Label className="px-4 mb-2">
-                        Tables
-                    </Label>
-                    <TableList />
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="h-16 px-4 flex items-center justify-between">
+        <div className="grow">
+          <Button size="lg" variant="ghost" className="!px-4">
+            <Database className={getTextColorClass(activeBase.color)} />{" "}
+            {activeBase.name}
+            <ChevronDown />
+          </Button>
         </div>
-    );
+        {isMobile ?
+          <Button
+            size="icon"
+            variant="ghost"
+            onClick={() => sidebarStatus.toggleMobile()}
+          >
+            <XIcon />
+          </Button>
+        : <Button
+            size="icon"
+            variant="ghost"
+            onClick={() => sidebarStatus.toggle()}
+          >
+            <ChevronsLeftIcon />
+          </Button>
+        }
+      </div>
+      <div className="flex flex-col gap-2 px-4">
+        <div className="py-2 w-full">
+          <SearchBoxPopup />
+        </div>
+        <div className="w-full flex flex-col gap-1 mb-4">
+          <Button variant="ghost" className="!px-4 w-full">
+            <PlusCircle />
+            <span className="grow text-left ">New table</span>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link to="." search={{ page: "overview" }} className="!px-4 w-full">
+              <LayoutDashboard />
+              <span className="grow text-left block">Overview</span>
+            </Link>
+          </Button>
+        </div>
+        <div className="w-full flex flex-col gap-1">
+          <Label className="px-4 mb-2">Tables</Label>
+          <TableList />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function TableList() {
-    const { tables, activeTableId: activeBaseId } = useTables();
+  const { tables, activeTableId: activeBaseId } = useTables();
 
-    const [expandedTables, setExpandedTables] = useState<Array<string>>(() => activeBaseId ? [activeBaseId] : [])
+  const [expandedTables, setExpandedTables] = useState<Array<string>>(() =>
+    activeBaseId ? [activeBaseId] : []
+  );
 
-    const handleToggleTable = (tableId: string) => {
-        setExpandedTables((prev) => {
-            if (prev.includes(tableId)) {
-                return prev.filter(id => id !== tableId);
-            } else {
-                return [...prev, tableId];
-            }
-        })
-    };
+  const handleToggleTable = (tableId: string) => {
+    setExpandedTables((prev) => {
+      if (prev.includes(tableId)) {
+        return prev.filter((id) => id !== tableId);
+      } else {
+        return [...prev, tableId];
+      }
+    });
+  };
 
-    return (
-        <div className="flex flex-col gap-1">
-            {
-                tables.map((table) => (
-                    <div
-                        key={table.id}
-                        className="flex flex-col gap-1"
-                    >
-                        <Button
-                            variant="ghost"
-                            className="!px-4 w-full !pr-1 group"
-                            asChild
-                        >
-                            <Link
-                                to="."
-                                search={{ page: 'table', tableId: table.id }}
-                            >
-                                <Table2 />
-                                <span className="grow text-left ">{table.name}</span>
-                                <div className="hidden group-hover:flex">
-                                    <Button variant="ghost" size="iconXs" >
-                                        <Ellipsis />
-                                    </Button>
-                                    <Button
-                                        variant="ghost"
-                                        size="iconXs"
-                                        onClick={(e) => {
-                                            handleToggleTable(table.id);
-                                            e.preventDefault();
-                                            e.stopPropagation();
-                                        }}>
-                                        {table.id === activeBaseId ? <ChevronDown /> : <ChevronRight />}
-                                    </Button>
-                                </div>
-                            </Link>
-                        </Button>
-                        {expandedTables.includes(table.id) && (
-                            <ViewList tableId={table.id} />
-                        )}
-                    </div>
-                ))
-            }
+  return (
+    <div className="flex flex-col gap-1">
+      {tables.map((table) => (
+        <div key={table.id} className="flex flex-col gap-1">
+          <Button variant="ghost" className="!px-4 w-full !pr-1 group" asChild>
+            <Link to="." search={{ page: "table", tableId: table.id }}>
+              <Table2 />
+              <span className="grow text-left ">{table.name}</span>
+              <div className="hidden group-hover:flex">
+                <Button variant="ghost" size="iconXs">
+                  <Ellipsis />
+                </Button>
+                <Button
+                  variant="ghost"
+                  size="iconXs"
+                  onClick={(e) => {
+                    handleToggleTable(table.id);
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }}
+                >
+                  {table.id === activeBaseId ?
+                    <ChevronDown />
+                  : <ChevronRight />}
+                </Button>
+              </div>
+            </Link>
+          </Button>
+          {expandedTables.includes(table.id) && <ViewList tableId={table.id} />}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 interface ViewListProps {
-    readonly tableId: string;
+  readonly tableId: string;
 }
 
 function ViewList({ tableId }: ViewListProps) {
-    const { getViewsByTableId } = useViews();
+  const { getViewsByTableId } = useViews();
 
-    const views = useMemo(() => getViewsByTableId(tableId), [getViewsByTableId, tableId]);
+  const views = useMemo(
+    () => getViewsByTableId(tableId),
+    [getViewsByTableId, tableId]
+  );
 
-    return (
-        <div className="flex flex-col gap-1">
+  return (
+    <div className="flex flex-col gap-1">
+      <Button variant="ghost" className="!px-10 w-full">
+        <Plus /> <span className="grow text-left">New view</span>
+      </Button>
+      {views.map((view) => (
+        <Button
+          key={view.id}
+          variant="ghost"
+          className="!pl-10 !pr-1 w-full group"
+          asChild
+        >
+          <Link
+            to="."
+            search={{ page: "view", viewId: view.id, tableId: tableId }}
+          >
+            <ViewIcon type={view.type} />
+            <span className="grow text-left">{view.name}</span>
             <Button
-                variant="ghost"
-                className="!px-10 w-full"
+              variant="ghost"
+              size="iconXs"
+              className="invisible group-hover:visible"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+              }}
             >
-                <Plus /> <span className="grow text-left">New view</span>
+              <Ellipsis />
             </Button>
-            {views.map((view) => (
-                <Button
-                    key={view.id}
-                    variant="ghost"
-                    className="!pl-10 !pr-1 w-full group"
-                    asChild
-                >
-                    <Link
-                        to="."
-                        search={{ page: 'view', viewId: view.id, tableId: tableId }}
-                    >
-                        <ViewIcon type={view.type} />
-                        <span className="grow text-left">{view.name}</span>
-                        <Button
-                            variant="ghost"
-                            size="iconXs"
-                            className="invisible group-hover:visible"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                e.stopPropagation();
-                            }}>
-                            <Ellipsis />
-                        </Button>
-                    </Link>
-                </Button>
-            ))}
-        </div>
-    );
+          </Link>
+        </Button>
+      ))}
+    </div>
+  );
 }
 
 function ViewIcon({ type }: Readonly<{ readonly type: string }>) {
-    switch (type) {
-        case 'grid':
-            return <Grid2x2 />;
-        case 'kanban':
-            return <SquareKanban />;
-        default:
-            return <Square />;
-    }
-
+  switch (type) {
+    case "grid":
+      return <Grid2x2 />;
+    case "kanban":
+      return <SquareKanban />;
+    default:
+      return <Square />;
+  }
 }
